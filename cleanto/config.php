c649cc4fbaf3bb8e70463f65fe0b13b41e7dbe53
akhilmokariy <akhilmokariya2192@gmail.com>
2021-11-23 20:11:34 +0530

    commit cleanto plugin

---
+++ cleanto/config.php
@@ -0,0 +1,9 @@
+<?php
+	class cleanto_myvariable{
+	
+	public $hostnames = "localhost";
+	public $username = "root";
+	public $passwords = "root";
+	public $database = "mdwlegmy_lat";
+	public $epcode = "8e58f24a-52cb-47ba-9559-3440c6cadaef";
+} ?>
